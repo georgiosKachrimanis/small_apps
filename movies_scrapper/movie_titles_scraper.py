b0ed9bb27fa82f3b1ed8a5478bae3546f73c1dc7
gekac <georgios.kachrimanis@icloud.com>
2023-11-26 18:46:42 +0100

    small fixes in movie_titles-scraper

--- movies_scrapper/movie_titles_scraper.py
+++ movies_scrapper/movie_titles_scraper.py
@@ -3,11 +3,8 @@ from bs4 import BeautifulSoup
 
 URL = "https://web.archive.org/web/20200518073855/https://www.empireonline.com/movies/features/best-movies-2/"
 
-# Write your code below this line 👇
-
 
 response = requests.get(url=URL,)
-# print(response.text)
 
 soup = BeautifulSoup(response.text, "html.parser")
 
